dd53eb2cbafcce30af8d902d136a1d330e6d3420
wanching_97 <wanching_97@hotmail.com>
2018-07-17 00:13:48 +0800

    added findMax method

--- src/FindMinimum/FindMin.java
+++ src/FindMinimum/FindMin.java
@@ -1,5 +1,6 @@
 package FindMinimum;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class FindMin {
@@ -12,6 +13,17 @@ public class FindMin {
         }
 
         return min;
+    }
+
+    public static <E extends Comparable<E>> E max (ArrayList<E> list) {
+        E max = list.get(0);
+
+        for(int i = 0; i < list.size(); i++) {
+            if(max.compareTo(list.get(i)) < 0)
+                max = list.get(0);
+        }
+
+        return max;
     }
 
     public static void main(String[] args) {
