88624b93026108cc2f2fc3a7b739a665491c11a9
wanching_97 <wanching_97@hotmail.com>
2018-07-16 17:09:43 +0800

    added genericstack class

---
+++ src/GenericStack.java
@@ -0,0 +1,31 @@
+public class GenericStack<E> {
+    private java.util.ArrayList<E> list = new java.util.ArrayList<E>();
+
+    public int getSize() {
+        return list.size();
+    }
+
+    public E peek() {
+        return list.get(getSize() - 1);
+    }
+
+    public void push(E o) {
+        list.add(o);
+    }
+
+    // Parameterized type is Double, then pop method return Double, no need extra casting
+    public E pop() {
+        E o = list.get(getSize() - 1);
+        list.remove(getSize() - 1);
+        return o;
+    }
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    @Override
+    public String toString() {
+        return "stack: " + list.toString();
+    }
+}
